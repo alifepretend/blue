57a01de05d0b02f2e1cd2342f5ee4db340ef3258
alifepretend <102198075+alifepretend@users.noreply.github.com>
2024-08-27 15:58:46 -0300

    1st commit

---
+++ NTCGraph/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "../libraries/IO.h"
+#include "../libraries/Math.h"
+#include "../libraries/Calibration.h"
+#include <ostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+IO io;
+
+int main() {
+	vector<vector<double>> value_list = io.parsecsv(IO::readFile("./temp_measurement.txt")); 
+
+	vector<vector<double>> trimmed_value_list = io.removeColumn(value_list, 2);
+
+	vector<vector<double>> transformed_value_list = io.applyFunction(trimmed_value_list, -1, &Calibration::esp32AdcToVoltage);
+
+  vector<vector<double>> resistance_value_list = io.applyFunction(transformed_value_list, 0, &Calibration::voltageInDivisorToResistance, 93.9, 3.3);
+  
+  resistance_value_list = io.applyFunction(resistance_value_list, 1, &Calibration::voltageInDivisorToResistance, 491, 3.3);
+
+  resistance_value_list = io.applyFunction(resistance_value_list, 0, &Calibration::pt100ResistanceToTemperature);
+  for (vector<double> line : resistance_value_list) {
+    string temp_line = "";
+
+    for (int i = 0; i < line.size(); i++) {
+      temp_line += to_string(line[i]);
+      if (i != line.size() - 1) {
+        temp_line += ",";
+      } else {
+        temp_line += "\n";
+      }
+    }
+
+    io.writeFile(temp_line, "./resistencia.txt");
+    cout << endl;
+  }
+  
+  return 1;
+}
+
---
+++ libraries/IO.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "serialib.h"
+
+#ifndef IO_H
+#define IO_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <codecvt>
+#include <locale>
+#include <functional>
+
+using namespace std;
+
+class IO
+{
+public:
+	struct Position{
+		unsigned int x = 0;
+		unsigned int y = 0;
+	};
+
+	void writeFile(string text, string path, bool append = 1);
+
+	void writeFileWithCurrentTime(string text, string path, bool append = 1);
+
+	// Retorna a lista de linhas em um arquivo.
+	static vector<string> readFile(string path);
+
+	struct Type {
+		static const int STRING = 0;
+		static const int INT = 1;
+		static const int DOUBLE = 2;
+	};
+
+	vector<vector<double>> parsecsv(vector<string> linelist);
+
+	double stringToDouble(string s);
+
+	vector<vector<double>> approximateGivenTheTime(vector<vector<double>>* reference_list, vector<vector<double>> signal_list);
+
+	vector<vector<double>> removeColumn(vector<vector<double>> matrix, int column_index);
+
+  // Passar -1 para o column_index aplica a função a todos os elementos da matriz.
+  vector<vector<double>> applyFunction(vector<vector<double>> matrix, int column_index, function<double(double)> fun);
+
+  vector<vector<double>> applyFunction(vector<vector<double>> matrix, int column_index, function<double(double, double, double)> fun, double arg1, double arg2);
+
+  vector<vector<double>> applyFunction(vector<vector<double>> matrix, int column_index, function<double(double, double, double, double)> fun, double arg1, double arg2, double arg3);
+
+
+	class Serial {
+	private:
+		serialib serial;
+	public:
+		char erroropening = 0;
+
+		// inicia a porta serial.
+		Serial(const char* serial_port, long int baud_rate);
+
+		// retorna 1 no caso de sucesso, 0 caso falhe.
+		int  receiveData(string* line);
+	};
+};
+
+#endif // !IO_H
